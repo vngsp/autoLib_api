ec10e954a305c2c2538665a40f3fa582f28af5a6
vngsp <viniciuspenhadev@gmail.com>
2026-01-24 18:12:15 -0300

    feat: configure CORS to allow frontend origin

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,19 @@ import express from 'express'
 import helmet from 'helmet'
 import mainRouter from './routes';
 import { setupSwagger } from './config/swagger';
+import cors from 'cors';
 
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 server.use('/', mainRouter);
+server.use(cors({
+    origin: [
+        'http://localhost:3000',
+    ],
+    methods: ['GET']
+}))
 
 setupSwagger(server);
 
